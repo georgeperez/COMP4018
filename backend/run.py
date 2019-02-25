45e49f718e0a0672e14e881ce09cd8c0bbc1511e
George Perez <george.perez.marrero@gmail.com>
2019-02-25 17:35:42 -0400

    Add MySQL boilerplate to run.py

--- backend/run.py
+++ backend/run.py
@@ -1,20 +1,21 @@
 import os
 from flask import Flask, render_template, jsonify
+from flask_cors import CORS
+from flaskext.mysql import MySQL
 from random import *
+
 app = Flask(__name__,
             static_folder = "../dist/static",
             template_folder = "../dist")
-@app.route('/api/random')
-def random_number():
-    response = {
-        'randomNumber': randint(1, 100)
-    }
-    return jsonify(response)
+CORS(app)
+
+mysql = MySQL()
+mysql.init_app(app)
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return render_template("index.html")
-
 
 if __name__=="__main__":
     app.run(host=os.getenv('IP', '0.0.0.0'), 
