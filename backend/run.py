c7ccc220efcdce66935ae622b5f6fdc76e4fb017
George Perez <george.perez.marrero@gmail.com>
2019-02-25 10:27:28 -0400

    Add names to Hello World App

---
+++ backend/run.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template, jsonify
+from random import *
+app = Flask(__name__,
+            static_folder = "../dist/static",
+            template_folder = "../dist")
+@app.route('/api/random')
+def random_number():
+    response = {
+        'randomNumber': randint(1, 100)
+    }
+    return jsonify(response)
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
+
+
+if __name__=="__main__":
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT',5000)),debug=True)
---
+++ front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <p>Hello World!</p>
+    <p>COMP4018 - Mamba Database Systems</p>
+    <p>George P&eacute;rez Marrero</p>
+    <p>Carlos L. Malav&eacute; Solares</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
