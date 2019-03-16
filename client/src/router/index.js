1a38c39e9b734a4ffaaa804162710d6059324a2e
George Perez <george.perez.marrero@gmail.com>
2019-03-16 01:06:37 -0400

    Add example code to add books

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Ping from '@/components/Ping';
+import Books from '@/components/Books';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      path: '/books',
+      name: 'Books',
+      component: Books,
+    },
+    {
+      path: '/ping',
+      name: 'Ping',
+      component: Ping,
     },
   ],
+  mode: 'history',
 });
